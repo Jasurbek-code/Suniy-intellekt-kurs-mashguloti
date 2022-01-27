57e8769d7cab49a3e61cd1d438545d5d3ea6d6e0
Jasurbek-code <66709683+Jasurbek-code@users.noreply.github.com>
2022-01-27 09:19:13 -0800

    misollar
    misollar uchun

--- begin_1-dars.py
+++ begin_1-dars.py
@@ -1,42 +1,83 @@
+
 #  1-2-misol.   Kvadratni perimetri va yuzasi
-a = 15;            print("P=", 4*a, "  S=", a**2)
+a = 14
+print("1-2-misol. ", "Peremetr=", 4*a, "  Yuza=", a**2)
 
 # 3-4-misol.    Tog'rito'rtburchak Perimetri va Yuzasi
-a,b = 10,8;        print("P=", 2*(a+b), "   S=", a*b)
+a, b = 10, 8
+print("3-misol. ", "Peremetr=", 2*(a+b), "   Yuza=", a*b)
 
 # 4-misol.      Aylanani diametri bor, Uzunligini topish
-d = 10;            print("L=", 3.14*d)
+d = 10
+print("4-misol. ", "L=", 3.14*d)
 
 # 5-misol.      Kubni hajmi
-a = 4;             print("V=", a**3)
+a = 4;              print("5-misol. ", "V=", a**3)
 
-# 6-misol.  Kubni to'la sirti
-a, b, c = 2,3,4;     print("Shar Sirti=", 2*(a*b + b*c + a*c))
+# 6-misol.  Paralelepepidning Hajmi va Yuzasi
+a, b, c = 2, 3, 4
+V = a*b*c
+S = 2*(a*b + a*c + b*c)
+print("6-misol.  \n\tShar hajmi=", V, "\n\tShar Sirti=", S)
 
 # 9-misol.      O'rta geometrigi
-a,b = 4,9;            print("O'rta geometrik=", pow(a*b, 1/2))
+a, b = 4, 9
+print("9-misol. ", "O'rta geometrik=", pow(a*b, 1/2))
+
+#  10-11-misol.    N!=0 va M!=0 sonni y-yig'indisi, k-ko'paytmasi, m-moduli
+N, M = 5, 7
+y = M + N;      k = M*N
+print("10-misol.  Yig'indisi=", y, "Kopaytmasi=", k, "Modullari: M=", abs(M), "N=", abs(N))
 
 # 12-misol.     Uchburchak gipotenuzasi va perimetri
-a,b = 3,4;           c= pow(a*a + b**2, 1/2)
-print("Gipotenuza c=", c,  "  Perimetri= a+b+c =", a + b + c)
+a,b = 3,4;              c= pow(a*a + b**2, 1/2)
+print("12-misol. ", "Gipotenuza c=", c,  "  Perimetri= a+b+c =", a + b + c)
 
 # 13-misol.     Markazda bo'lmagan 2 ta Radius berilgan (R1 > R2). Yuzalari S1, S2
-R1, R2 = 15, 10;        print("S1=", 3.14*R1, "  S2=", R2*3.14)
+R1, R2 = 15, 10
+print("13-misol.  S1=", 3.14*R1, "  S2=", R2*3.14, "\n\tYuzalar ayirmasi=", 3.14*(R1 - R2))
+
+# 14-misol.         L berilgan. Aylana radiusi va yuzasi
+L = 26
+R = L/2*3.14
+S = 3.14*(R**2)
+print("14-misol.  R=", R, "S=", S)
+
+# 15-misol.         S aylana yuzasi berilgan. diametri va radiusi
+S = 25
+R = pow(S/3.14, 0.5)
+d = 2*R
+print("15-misol.   R=", R, "d=", d)
+
+# 16-misol.
+x1, x2 = 9, 5
+print("16-misol.  Ikki nuqta orasidagi masofa=", x1 - x2)
 
 # 17-misol.     AC va BC kesmaning uzunligi va kesmalar uzunligining yig'indisi         ????????????????????????????????????
 A,B,C = 3,4,5;          AC = A+B;   BC = B+C
-print("AC=", AC, "  BC=", BC, "  Yig'indisi=", AC+BC)
+print("17-misol.  AC=", AC, "  BC=", BC, "  Yig'indisi=", AC+BC)
 
-# 20-misol.     Tekislikda berilgan nuqtalar orasidagi masofa
-x1,x2,y1,y2 = 2,3,4,5;          L = pow(((x2-x1)**2 + (y2-y1)**2), 1/2)
-print("Masofa L=", L)
+# 20-misol.     Tekislikda berilgan koordinata orqali nuqtalar orasidagi masofa
+x1,x2, y1,y2 = 2,3,4,5;          L = pow(((x2-x1)**2 + (y2-y1)**2), 1/2)
+print("20-misol. ", "Masofa L=", L)
+
+# 21-misol.         Koordinata orqali uchburchak yuzasini topish
+x1,y1, x2,y2, x3,y3 = 2,3, 7,6, 12,4
+a = (pow((x1-x2)**2 + (y1-y2)**2, 0.5))
+b = (pow((x3-x2)**2 + (y3-y2)**2, 0.5))
+c = (pow((x1-x3)**2 + (y1-y3)**2, 0.5))
+
+p = (a + b + c)/2
+yuza = pow(p*(p-a)*(p-b)*(p-c), 1/2)
+
+print("21-misol.  Peremetri=", p, "Yuzasi=", yuza)
 
 # 22-misol.         Sonni qiymatini almashtirish
 A, B = 5, 6
 A = A + B
 B = A - B
 A = A - B
-print("A=", A, "  B=", B)
+print("22-misol. ", "A=", A, "  B=", B)
 
 # 23-misol.         A => B ga, B => C ga, C => A ga o'zlashtirilsin
 A, B, C = 4,5,6
@@ -44,40 +85,59 @@ A = A + B + C
 B = A - B - C
 C = A - B - C
 A = A - B - C
-print("A=",A, "  B=",B, "  C=",C)
+print("23-misol. ", "A=",A, "  B=",B, "  C=",C)
 
-# 25-misol.     x ni qiymati berilgan.
-x=2;          y = 3*x**6 - 6*x*x - 7;       print("y=", y)
+# 25-misol.     x ni qiymati berilgan. funksiya
+x=2;          y = 3*x**6 - 6*x*x - 7
+print("25-misol. ", "y=", y)
 
-# 27-misol.
-A=2;            print("A^2=", A**2, "  A^4=", A**4, "  A^8=", A**8)
+# 27-28-misol.
+A=2;            print("27-28-misol. ", "A^2=", A**2, "  A^3=", A**3,"  A^4=", A**4,"  A^5=", A**5, "  A^8=", A**8)
 
 # 29-misol.     radianga o'tqazuvchi
-gradus = 180;       print("Radian=", 3.14*gradus/180)
+gradus = 100;       print("29-misol. ", "Radian=", 3.14*gradus/180)
+
+# 30-misol.     radianga o'tqazuvchi
+radian = 5;       print("30-misol. ", "Gradus=", radian*180/3.14)
 
 # 31-misol.       Temperaturani Farengeytda chiqarish
 Tc = 45;         Tf = (Tc - 32)*5/9
-print("Farengeyt Tf=", Tf)
+print("31-misol. ", "Farengeyt Tf=", Tf)
+
+# 33-34-misol.        konfet narxi
+X_kg = 2;    A_sum = 2000
+Y_kg = 4    #int(input("Necha kg:"))
+Y_sum = Y_kg*A_sum/X_kg
+print("33-34-misol.  Sum:", Y_sum)
 
 # 35-misol.     Qayiq tezligi V, oqim tezligi U, daryo oqim buyicha harakati vaqti T1, oqimga qarshi T2.
 #                qayiqni yurgan S yuli aniqlansin
-V = 20; U = 2;  T1=1; T2=3;                         S = (V+U)*T1 + (V-U)*T2
-print("Yurgan yuli  S=", S)
+V = 20; U = 2;  T1=1; T2=3
+S = (V+U)*T1 + (V-U)*T2
+print("35-misol. ", "Yurgan yuli  S=", S)
 
 # 36-misol.     v1, v2 tezlikli mashinalar S masofada,uzoqlashmoqda, t vaqtdan keyin ular orasidagi masofa.
-v1, v2 = 2,3; t = 2; S = 30;                      L = t*(v1 + v2) + S
-print("Uzoqlashganda masofa L=", L)
+v1, v2 = 2,3;  t = 2; S = 30
+L_uzoq = t*(v1 + v2) + S
+L_yaqin = S - t*(v1 + v2)
+print("36-misol. ", "Uzoqlashganda masofa L=", L_uzoq)
+print("37-misol. ", "Yaqinlashganda masofa L=", L_yaqin)
 
 # 38-misol.         chiziqli tenglama
-A, B = 4,6;            x = - (B/A);              print("x=", x)
+A, B = 4,6;            x = - (B/A)
+print("38-misol. ", "x=", x)
 
 # 39-misol.         diskriminant topish
 A,B,C = 4,5,6
 D = B*B - 4*A*C;   x1= -B + (D)**(1/2)/(2*A);   x2= -B - (D)**(1/2)/(2*A)
-print("D=", D,"  x1=",x1, "  x2=",x2)
+print("39-misol. ", "D=", D,"  x1=",x1, "  x2=",x2)
 
 # 40-misol.
 A1, B1, C1, A2, B2, C2 = 2,3,4,5,6,7
 D = (A1*B2 - A2*B1);   x = (C1*B2 - C2*B1)/D; y = (A1*C2 - A2*C2)/D
-#A1*x + B1*y = C1
-#A2*x + B2*y = C2
+print("40-misol.")
+print("   C1=", A1*x + B1*y)        #A1*x + B1*y = C1
+print("   C2=", A2*x + B2*y)        #A2*x + B2*y = C2
+
+
+
--- integers_2-dars.py
+++ integers_2-dars.py
@@ -8,16 +8,45 @@ M = 1532;       tonna= M/1000;          print("tonna=", tonna)
 bayt = 1024;        kb = bayt/1024;         print("kb=", kb)
 
 # 4-misol.
-A, B = 5, 4;        #                   ?????????????????????????????????????????????????????????
+A, B = 5, 4
+print("A kesmada B ni ", A//B, " marta joylashtirish mumkin.")
 
 # 5-misol.
-A, B = 9, 5;        #                    ????????????????????????????????????????????????????????
+A, B = 9, 5
+print("A kesmada B kesmani joylashmagan qismi ", A - A//B, "ta")
 
-# 7-misol
-a = 56;        onlik=a//10;   birlik=a%10;              print("son yig'indisi=", onlik + birlik)
+# 6-7-misol
+a = 56;           onlik=a//10;   birlik=a%10
+print(onlik, " + ", birlik, "=", onlik + birlik)
+
+# 9-misol.
+A = 235
+yuz, on, bir = A//100, A//10%10, A%10
+print("Yuzlar xonasi=", yuz)
+
+# 10-misol.
+print("Birlik xona=", bir, " O'nlik xona=", on)
+
+# 11-misol.
+print("uch xonali raqam yig'indisi=", yuz + on + bir)
+
+# 12-misol.
+print("Teskari tartib: ", bir*100 + on*10 + yuz)
+
+# 13-misol.
+print("O'ngdan chapga: ", on*100 + bir*10 + yuz)
+
+# 14-misol.
+print("Chapdan o'ngga: ", bir*100 + yuz*10 + on)
+
+# 15-misol.
+print("O'nlok xona bilan yuzlik xona almashishi:", on*100 + yuz*10 + bir)
+
+# 16-misol.
+print("O'nlok xona bilan birlik xona almashishi:", yuz*100 + bir*10 + on)
 
 # 17-misol
-a = 153113456789;           yuzlik= a % 1000 // 100;          print("yuzlik=", yuzlik)
+a = 153113456789;           yuzlik= a // 100 % 10;          print("yuzlik=", yuzlik)
 
 # 18-misol.
 a = 153113456789;           minglik= a % 10000 // 1000;          print("minglik=", minglik)
@@ -32,7 +61,9 @@ sek= 3600;        soat = sek//3600;      print("soat=", soat)
 N= 121;           min = N//60;           sek = N % 60;               print(min, "min", sek, "sekund")
 
 # 23-misol.
-N = 3670;         soat = N // 3600;      min = N % 3600 // 60;  sek = N % 60;        print(soat, "soat", min, "min", sek, "sekund")
+N = 3670
+soat = N // 3600;      min = N % 3600 // 60;    sek = N % 60
+print(soat, "soat", min, "min", sek, "sekund")
 
 # 24-misol.
 K = 16;           kun = K % 7;               print("kun=", kun)
@@ -48,5 +79,4 @@ A,B,c = 5,3,2;      soni = A//c + B//c;          print(soni, "ta")
 
 # 30-misol.
 yil = 1900;         yillik = yil // 100 + 1;       print(yillik, "yillikda")
-
 
