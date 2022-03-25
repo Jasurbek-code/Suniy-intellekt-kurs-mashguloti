7dd34955f08bc5a2556baff95893a2f4ca831e79
Jasurbek-code <66709683+Jasurbek-code@users.noreply.github.com>
2022-03-25 10:57:11 -0700

    misollar

---
+++ oy-3-4/set_31-dars.py
@@ -0,0 +1,51 @@
+#  1-misol.
+m, n = 5, 3
+a = set([])
+for i in range(m):
+    a.add(i)
+    for j in range(n):
+        a.add(j)
+print(a)
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+#  -misol.
+
+
+
+
