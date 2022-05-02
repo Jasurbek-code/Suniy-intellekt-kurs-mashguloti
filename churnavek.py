83b49888be875e3003b90780b7341de0a5e622f6
Jasurbek-code <66709683+Jasurbek-code@users.noreply.github.com>
2022-05-02 04:27:18 -0700

    sak

--- churnavek.py
+++ churnavek.py
@@ -1,31 +1,3 @@
 
 
 
-
-
-#n = 10  #int(input("n= "))
-#a = [random.randint(0, n) for i in range(n)]
-#print("a:  ", a)
-#
-
-
-
-
-
-
-
-x = [3,5,5,5,6,4,7,7,6,6,7,2,2]
-y = [3,5,5,5,6,4,7,7,6,6,7,2,2]
-k = 7
-print(x)
-print(x)
-
-
-
-
-# git branch -m main ai
-# git fetch origin
-# git branch -u origin/ai ai
-# git remote set-head origin -a
-
-
